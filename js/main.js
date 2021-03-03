2cae48718543116e0641da56e447a3050d3ba9e2
OleksandrVolodimirovich <61995509+OleksandrVolodimirovich@users.noreply.github.com>
2021-03-03 10:13:32 +0200

    виведення карток магазинів і товарів на сторінку
    - авторизація при кліку на магазин
    - добавлення HTML через js
    -swiper slider

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
+'use strict';
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-//start
-
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
@@ -18,22 +10,23 @@ const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('fastDelivery'); //замімть пустого значення змінній login присвоюэмо значення з localStorage
 
-//console.log(buttonAuth);
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
 
-function toogleModalAuth(){
+function toogleModalAuth(){ //ф-ція виклику вікна авторизизації
   loginInput.style.borderColor = ''; //очищає поле логін від червоного бордера
   modalAuth.classList.toggle('is-open');
 }
-
-buttonAuth.addEventListener('click', function(){
-  console.log('Hello');
-});
-
-buttonAuth.addEventListener('click', toogleModalAuth);
-closeAuth.addEventListener('click', toogleModalAuth)
 
 function authorized(){
   function logOut() {  //ф-ція виходу з авторизації
@@ -56,10 +49,6 @@ function authorized(){
 
   buttonOut.addEventListener('click', logOut);
 
-}
-
-function maskInput(string) { 
-  return !!string.trim()      //видаляє пробіли
 }
 
 function notAuthorized() {
@@ -95,6 +84,125 @@ function checkAuth() { //ф-ція провірки авторизації
     notAuthorized();
   }
 }
+
+function createCardRestaurant(){
+  const card = `
+    <a class="card card-restaurant">
+      <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title">Пицца плюс</h3>
+          <span class="card-tag tag">50 мин</span>
+        </div>
+        <div class="card-info">
+          <div class="rating">
+            4.5
+          </div>
+          <div class="price">От 900 ₽</div>
+          <div class="category">Пицца</div>
+        </div>
+      </div>
+    </a>
+  `;
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+
+}
+
+function createCardGood() {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.insertAdjacentHTML('beforeend', `
+      <img src="img/pizza-plus/pizza-oleole.jpg" alt="image" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">Пицца Оле-Оле</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">Соус томатный, сыр «Моцарелла», черри, маслины, зелень, майонез
+          </div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">440 ₽</strong>
+        </div>
+      </div>
+    `);
+
+    cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+function openGoods(event) {
+
+  const target = event.target;
+  const restaurant = target.closest('.card-restaurant');
+
+  if(restaurant){
+      
+      if(login){
+        cardsMenu.textContent = '';  //щоб не дублювалися піцци, очистка відбувається після натискання на ресторан
+        containerPromo.classList.add('hide');
+        restaurants.classList.add('hide');
+        menu.classList.remove('hide');
+        createCardGood();
+      } else {
+        toogleModalAuth();
+      }
+    }
+}
+
+function maskInput(string) { 
+  return !!string.trim()      //видаляє пробіли
+}
+
+
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+cardsRestaurants.addEventListener('click', openGoods);
+
+logo.addEventListener('click', function() { //ф-ція при натисканні на лого повертає головну сторінку
+  containerPromo.classList.remove('hide');
+    restaurants.classList.remove('hide');
+    menu.classList.add('hide');
+})
+
+
 checkAuth();
+createCardRestaurant();
+
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  autoplay: true,
+});
+
+// buttonAuth.addEventListener('click', function(){
+//   console.log('Hello');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
